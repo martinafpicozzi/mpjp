7ff8acf3401cb055797d4365b10b72f06bb40a19
Martina Picozzi <martinafrancesca.picozzi1@gmail.com>
2019-12-10 17:41:58 +0100

    Ultimo commit

--- src/main/java/s044/Exercise3.java
+++ src/main/java/s044/Exercise3.java
@@ -1,15 +1,18 @@
 package s044;
 
-public class Exercise3 { // quanti sono i numeri pari nell'array
+public class Exercise3 { // quanti sono i numeri pari nell'array e somma dei numeri dispari
 	public static void main(String[] args) {
-		int[] array = { 7, 3, 1, 2, 4, 8 };
+		int[] array = { 7, 3, 1, 2, 4, 76, 9, 6 };
 		int counter = 0;
+		int sumodds = 0;
 
 		for (int i = 0; i < array.length; i++) {
 			if (array[i] % 2 == 0) {
-				counter = ++ counter; 
+				++counter; 
+			else
+				sumodds += array[i];
 			}
 		}
-		System.out.println("Your array has " + counter + " even numbers");
+		System.out.println("Your array has " + counter + " even number(s)");
+		}
 	}
-}
