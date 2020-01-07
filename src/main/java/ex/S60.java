af0576f2a77274711dcec22f8fa893ed15777461
Martina Picozzi <martinafrancesca.picozzi1@gmail.com>
2020-01-07 17:43:17 +0100

    Commit 07/01

--- src/main/java/ex/S60.java
+++ src/main/java/ex/S60.java
@@ -1,5 +1,7 @@
 package ex;
 
+import java.lang.reflect.Array;
+
 public class S60 {
 	/**
 	 * Reverse a string
@@ -65,23 +67,43 @@ public class S60 {
 	 * @return a new array holding the same elements of input, in reversed order
 	 */
 	public static int[] reverse(int[] data) {
-		int[] result = new int[0];
-
-		// TODO
-
+		int[] result = new int[data.length];
+		int j = 0;
+		for (int i = data.length -1; i >= 0; i --) { // si parte dalla fine dell'array
+			result [j] = data [i];
+			j++;
+		}
 		return result;
 	}
 
+	/**
+	 Seconda possibilità di codice* 
+	 */
+	public static int[] reverse2(int[] data) {
+		int[] result = new int[data.length];
+		for (int i = data.length -1, j = 0; i >= 0; j++, i --) {
+			result [j] = data [i];
+		}
+		return result;
+	}
+	
 	/**
 	 * Calculate the average
 	 * 
 	 * @param data
 	 * @return the average
 	 */
-	public static double average(int[] data) {
-		// TODO
-		return 0;
+	public static double average(int[] data) { // attenzione: se faccio divisione tra interi ottengo un intero. Deve
+												// ritornare un double
+		double sum = 0;
+		for (int i = 0; i < data.length; i++) {
+			sum += data[i];
+		}
+		return sum / data.length;
 	}
+
+//	for (int value : data) { // con il for each
+//		sum += value;
 
 	/**
 	 * Find the largest value
@@ -89,8 +111,22 @@ public class S60 {
 	 * @param data
 	 * @return the largest value
 	 */
-	public static int max(int[] data) {
-		// TODO
-		return 0;
+	public static int max(int[] data) { // si può usare l'Integer, classe che contiene la proprietà del valore più piccolo
+
+//		if (data.length == 0) { // se data è vuoto (null o 0, deve tirare un'eccezione)
+//			return biggest;
+//		}
+//		if (data == null) {         // NON E' CHIARO
+//			return;
+//		}
+		
+		int biggest = data[0];
+		for (int i = 1; i < data.length; i++) {
+			if (biggest < data[i]) {
+				biggest = data[i];
+			}
+		}
+		return biggest;
 	}
 }
+// int biggest = Integer.MIN_VALUE; 
